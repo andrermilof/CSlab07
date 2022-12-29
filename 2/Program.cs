3cf2ec4f407a85e5de8b669cbf2c267c941a0614
andrermilof <andrermilof@gmail.com>
2022-12-29 08:54:12 +0300

    Добавьте файлы проекта.

---
+++ 2/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using lab07;
+
+namespace _2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument xDocument = new XDocument();
+
+            Assembly assembly = Assembly.Load(new AssemblyName("lab07"));
+
+            Type[] types = assembly.GetTypes();
+            XElement[] xElements = new XElement[types.Length];
+
+            XElement fields, props, methods, attrs;
+            BindingFlags flags = BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly;
+            for (int i = 0; i < types.Length; i++)
+            {
+                xElements[i] = new XElement(types[i].Name);
+
+                Attribute[] at = Attribute.GetCustomAttributes(types[i]);
+                attrs = new XElement("attributes");
+                foreach(var a in at)
+                {
+                    Comment cm;
+                    if (a is Comment)
+                    {
+                        cm = (Comment)a;
+                        attrs.Add(cm.com + " ");
+                    }
+                }
+                xElements[i].Add(attrs);
+
+                if (types[i].IsClass)
+                {
+                    if (types[i].IsAbstract)
+                        xElements[i].Add(new XAttribute("type", "abstract"));
+
+                    for (int j = 0; j < types.Length; j++)
+                        if (types[i].IsSubclassOf(types[j]))
+                        {
+                            xElements[i].Add(new XAttribute("type", $"subclass of {types[j].Name}"));
+                            break;
+                        }
+                   
+                    FieldInfo[] fieldinfo = types[i].GetFields(flags);
+                    PropertyInfo[] propinfo = types[i].GetProperties(flags);
+                    MethodInfo[] methinfo = types[i].GetMethods(flags);
+                    
+                    if (fieldinfo.Length != 0)
+                    {
+                        fields = new XElement("fields");
+                        foreach (var field in fieldinfo)
+                            fields.Add(field.Name + ' ');
+
+                        xElements[i].Add(fields);
+                    }
+
+                    if (propinfo.Length != 0)
+                    {
+                        props = new XElement("properties");
+                        foreach (var prop in propinfo)
+                            props.Add(prop.Name + ' ');
+
+                        xElements[i].Add(props);
+                    }
+
+                    if (methinfo.Length != 0)
+                    {
+                        methods = new XElement("methods");
+                        foreach (var prop in methinfo)
+                            methods.Add(prop.Name + ' ');
+
+                        xElements[i].Add(methods);
+                    }
+                }
+
+                if (types[i].IsEnum)
+                {
+                    xElements[i].Add(new XAttribute("type", "enum"));
+                    FieldInfo[] fieldinfo = types[i].GetFields();
+                    fields = new XElement("fields");
+                    foreach (var field in fieldinfo)
+                        fields.Add(field.Name + ' ');
+
+                    xElements[i].Add(fields);
+                }
+            }
+
+            XElement all = new XElement("lab07");
+            foreach(var el in xElements)
+                all.Add(el);
+
+            xDocument.Add(all);
+            xDocument.Save("all.xml");
+        }
+    }
+}
---
+++ lab07/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab07
+{
+    public class Comment : Attribute
+    {
+        public string com { get; }
+
+        public Comment(string com)
+        {
+            this.com = com;
+        }
+    }
+
+    public enum eClassificationAnimal
+    {
+        Herbivores,
+        Carnivores,
+        Omnivores
+    }
+
+    public enum eFavouriteFood
+    {
+        Meat,
+        Plants,
+        Everything
+    }
+
+    [Comment("Animal")]
+    public abstract class Animal
+    {
+        public string Name { get; set; }
+        
+        public string Country { get; set; }
+
+        public string HideFromOtherAnimals { get; set; }
+
+        public string WhatAnimal { get; set; }
+
+        public eClassificationAnimal ClassificationAnimal;
+        
+
+        public Animal() { }
+
+        public void Deconstruct() { }
+
+        public eClassificationAnimal GetClassificationAnimal()
+        {
+            return ClassificationAnimal;
+        }
+        
+        abstract public eFavouriteFood GetFavouriteFood();
+
+        abstract public string SayHello();
+    }
+
+    [Comment("Cow")]
+    public class Cow : Animal
+    {
+        public Cow() { }
+        override public eFavouriteFood GetFavouriteFood()
+        {
+            return eFavouriteFood.Plants;
+        }
+        override public string SayHello()
+        {
+            return "mo";
+        }
+    }
+
+    [Comment("Lion")]
+    public class Lion : Animal
+    {
+        public Lion() { }
+        override public eFavouriteFood GetFavouriteFood()
+        {
+            return eFavouriteFood.Meat;
+        }
+        override public string SayHello()
+        {
+            return "rr";
+        }
+    }
+
+    [Comment("Pig")]
+    public class Pig : Animal
+    {
+        public Pig() { }
+        override public eFavouriteFood GetFavouriteFood()
+        {
+            return eFavouriteFood.Everything;
+        }
+        override public string SayHello()
+        {
+            return "hru";
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
